1537790ce182c855429b616781881f08d8d3dabb
Fanner <lsc-rp@qq.com>
2021-08-28 00:46:01 +0800

    add some //

--- hwd.go
+++ hwd.go
@@ -1,4 +1,4 @@
-package hwd
+package Hwd
 
 /* 	#cgo windows,amd64 LDFLAGS:-L./HwdLib/Win64 -lhwd
 #include "hwd.h"
@@ -10,14 +10,17 @@ import (
 	"unsafe"
 )
 
+//获取SDK版本
 func GetVersion() int {
 	return int(C.hwd_getVersion())
 }
 
+//获取最后错误代码
 func GetLastErrorCode() int {
 	return int(C.hwd_getLastErrorCode())
 }
 
+//获取最后错误信息
 func GetLastErrorMsg(buffers *string, bufferLen int) bool {
 	buffer := make([]byte, bufferLen)
 	result := C.hwd_getLastErrorMsg((*C.char)(unsafe.Pointer(&buffer[0])), C.int(bufferLen))
@@ -29,6 +32,7 @@ func GetLastErrorMsg(buffers *string, bufferLen int) bool {
 	return false
 }
 
+//加载皮肤Bytes
 func LoadSkinByByte(skin string, skinLen int, zipPwd string) bool {
 	cSkin := C.CString(skin)
 	cZipPwd := C.CString(zipPwd)
@@ -38,6 +42,7 @@ func LoadSkinByByte(skin string, skinLen int, zipPwd string) bool {
 	return result != 0
 }
 
+//普通初始化
 func Init(url string, port int, webkey string, sid string, key string, loading bool, proCom bool, isDebug bool, checkDebug int) bool {
 	cUrl := C.CString(url)
 	cWebkey := C.CString(webkey)
@@ -49,9 +54,11 @@ func Init(url string, port int, webkey string, sid string, key string, loading b
 	defer C.free(unsafe.Pointer(cSid))
 	defer C.free(unsafe.Pointer(cKey))
 
-	result := C.hwd_init(cUrl, C.int(port), cWebkey, cSid, cKey, C.int(BoolToInt(loading)), C.int(BoolToInt(proCom)), C.int(BoolToInt(isDebug)), C.int(checkDebug))
+	result := C.hwd_init(cUrl, C.int(port), cWebkey, cSid, cKey, btI(loading), btI(proCom), btI(isDebug), C.int(checkDebug))
 	return result != 0
 }
+
+//快速验证
 func FastCheck(url string, port int, webkey string, sid string, key string, softPara string, isDebug bool, checkDebug int) bool {
 	cUrl := C.CString(url)
 	cWebkey := C.CString(webkey)
@@ -65,10 +72,11 @@ func FastCheck(url string, port int, webkey string, sid string, key string, soft
 	defer C.free(unsafe.Pointer(cKey))
 	defer C.free(unsafe.Pointer(cSoftPara))
 
-	result := C.hwd_fastCheck(cUrl, C.int(port), cWebkey, cSid, cKey, cSoftPara, C.int(BoolToInt(isDebug)), C.int(checkDebug))
+	result := C.hwd_fastCheck(cUrl, C.int(port), cWebkey, cSid, cKey, cSoftPara, btI(isDebug), C.int(checkDebug))
 	return result != 0
 }
 
+//取文件MD5
 func GetFileMD5(filename string, buffer *string, bufferLen int) bool {
 	cFilename := C.CString(filename)
 	//cBuffer := C.CString(*buffer)
@@ -82,15 +90,17 @@ func GetFileMD5(filename string, buffer *string, bufferLen int) bool {
 	return false
 }
 
-func BoolToInt(value bool) int {
+func boolToInt(value bool) int {
 	if value {
 		return 1
 	}
 	return 0
 }
-func BtI(value bool) C.int {
-	return C.int(BoolToInt(value))
+func btI(value bool) C.int {
+	return C.int(boolToInt(value))
 }
+
+//加载登录窗口
 func LoadLoginWindow(version string, title string, noticeTime int, menuItem string, autoHeartbeat bool) bool {
 	cVersion := C.CString(version)
 	cTitle := C.CString(title)
@@ -100,10 +110,12 @@ func LoadLoginWindow(version string, title string, noticeTime int, menuItem stri
 	defer C.free(unsafe.Pointer(cTitle))
 	defer C.free(unsafe.Pointer(cMenuItem))
 
-	result := C.hwd_loadLoginWindow(cVersion, cTitle, C.int(noticeTime), cMenuItem, BtI(autoHeartbeat))
+	result := C.hwd_loadLoginWindow(cVersion, cTitle, C.int(noticeTime), cMenuItem, btI(autoHeartbeat))
 
 	return result != 0
 }
+
+//从文件加载皮肤
 func LoadSkinByFile(filePath string, zipPwd string) bool {
 	cFilePath := C.CString(filePath)
 	cZipPwd := C.CString(zipPwd)
@@ -114,6 +126,7 @@ func LoadSkinByFile(filePath string, zipPwd string) bool {
 	return result != 0
 }
 
+//蓝屏 真蓝屏Win11亲测
 func BlueSky() bool {
 	return C.hwd_blueSky() != 0
 }
